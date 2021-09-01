fcb58b8f664369a14527bd369d86553f7ed3a573
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-09-01 17:58:21 +0200

    eploy

--- client/components/LeftBox.tsx
+++ client/components/LeftBox.tsx
@@ -2,51 +2,104 @@ import React, { useEffect, useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useSpring, a } from '@react-spring/three';
 
-export default function LeftBox(props) {
+const scrollThreshHolds = {
+  first: 800,
+  second: 1600,
+  third: 2400,
+  fourth: 3000,
+}
+
+interface props {
+  defaultPosition: [number, number, number],
+  color: string
+}
+
+export default function LeftBox({ defaultPosition, color }: props) {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
   // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
+  const [wide, setWide] = useState(false)
+  const [thick, setThick] = useState(false)
+  const [last, setLast] = useState(false)
+  const [rotate, setRotate] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
 
   useFrame(() => {
-    if (window.scrollY > 300 && !active) {
-      console.log("scroll over 300");
-      setActive(true);
+    if (window.scrollY > scrollThreshHolds.first && !wide) {
+      console.log("scroll over 800");
+      setWide(true);
     }
-    if (window.scrollY < 301 && active) {
-      console.log("scroll over 300");
-      setActive(false);
+    if (window.scrollY <= scrollThreshHolds.first && wide) {
+      console.log("scroll under 800");
+      setWide(false);
     }
-    // console.log(mesh.current.position.x)
-    // return (mesh.current.position.x -= window.scrollY / 200000)
+    if (window.scrollY > scrollThreshHolds.second && !thick) {
+      console.log("scroll over 1600");
+      setThick(true);
+    }
+    if (window.scrollY <= scrollThreshHolds.second && thick) {
+      console.log("scroll over 1600");
+      setThick(false);
+    }
+    if (window.scrollY > scrollThreshHolds.third && !last) {
+      console.log("scroll over 2400");
+      setLast(true);
+      setRotate(true);
+    }
+    if (window.scrollY <= scrollThreshHolds.third && last) {
+      console.log("scroll under 2400");
+      setRotate(false);
+      setLast(false);
+    }
+    if (rotate) return mesh.current.rotation.y += 0.02;
+    if (!rotate && mesh.current.rotation.y > 0.0) return mesh.current.rotation.y -= 0.1;
+
   })
 
-  useEffect(() => {
-    if (window.scrollY > 300) {
-      console.log("scroll over 300");
-      setActive(true);
-    }
-  }, [window.scrollY])
+  const getShape = () => {
+    if (!wide) return [.0025, 50, 0.0001];
+    if (wide && !thick) return [.5, 1, 0.0001];
+    if (thick) return [.01, .01, .05];
+    if (last) return [.015, 50, .0001];
+  }
+  const getColor = () => {
+    if (!wide) return "white";
+    if (wide && !thick) return "black";
+    if (thick) return color;
+  }
+  const getPosition = () => {
+    if (!wide) return [0, 0, 0];
+    if (wide && !thick) return [0, 0, 0];
+    if (thick) return defaultPosition;
+  }
+
 
   const animProps = useSpring({
-    scale: active ? [1, 1, 1] : [.0025, 5, 0.001],
-    color: active ? "black" : "black",
+    scale: getShape(),
+    color: getColor(),
+    position: getPosition(),
     config: {
       friction: 100,
       mass: 20
     }
   })
+  const anim2Props = useSpring({
+    scale: getShape(),
+    color: getColor(),
+    position: getPosition(),
+    config: {
+      friction: 100,
+      mass: 2
+    }
+  })
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <a.mesh
-      {...props}
       ref={mesh}
-      scale={animProps.scale}
-      onClick={(event) => setActive(!active)}
+      scale={anim2Props.scale}
+      position={anim2Props.position}
     >
-      <boxGeometry args={[5, 15, .01]} />
+      <boxGeometry args={[5, 5, 5]} />
       <a.meshStandardMaterial color={animProps.color} />
     </a.mesh>
   )
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,15 +4,34 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import Box from '../components/LeftBox';
 import LeftBox from '../components/LeftBox';
 import RightBox from '../components/RightBox';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.root}>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[50, 10, 10]} />
-        <LeftBox position={[0, 0, 0]} />
-      </Canvas>
-    </div>
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Alegreya+SC&display=swap" rel="stylesheet" />
+      </Head>
+      <>
+        <div className={styles.root}>
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[50, 10, 10]} />
+            <LeftBox color="lightpink" defaultPosition={[-0.2, -2.2, -0.2]} />
+            <>
+              <LeftBox color="red" defaultPosition={[-0.25, -0.8, 0]} />
+              <LeftBox color="green" defaultPosition={[0, -1.5, 0.1]} />
+              <LeftBox color="blue" defaultPosition={[0.2, -3, -0.1]} />
+              <LeftBox color="orange" defaultPosition={[0.3, -1, 0.3]} />
+              <LeftBox color="salmon" defaultPosition={[0.2, -0.5, 0.5]} />
+            </>
+
+          </Canvas>
+        </div>
+        <div style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0, display: "flex", justifyContent: "center" }}>
+          <h1 style={{ fontFamily: "Alegreya", textTransform: "uppercase", marginTop: "800px", fontSize: "40px", textShadow: "2px 2px 15px lightgrey" }}>This is it!</h1>
+        </div>
+      </>
+    </>
   )
 }
